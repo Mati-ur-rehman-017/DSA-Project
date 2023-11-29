2437c7a3acd157efba4db1bd1b7a13ea99b3ec33
Shar-jeel-Sajid <sharjeelsajid09@gmail.com>
2023-11-29 20:13:08 +0500

    Metadata File Made

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include "nlohmann/json.hpp"
 #include "src/english_stem.h"
 #include <chrono>
+#include <cstddef>
 #include <cstring>
 #include <fstream>
+#include <ios>
 #include <iostream>
 #include <map>
 #include <string.h>
@@ -28,51 +30,61 @@ void split(const string &s, unordered_map<string, int> &words) {
 
 // corrected version
 void forward_index(string a) {
-  ifstream file(a + ".json");
-  if (!file.is_open()) {
+  ifstream file(a + ".json"), count("count.txt");
+  if (!file.is_open() || !count.is_open()) {
     std::cerr << "Failed to open the file." << endl;
     return;
   }
-  // Parsing json file
-  nlohmann::json jsonData;
-  file >> jsonData;
-
-  ofstream file_output("index.json"), meta_data("metadata.json");
-  if (!file_output.is_open() || !meta_data.is_open()) {
-    std::cerr << "Failed to open the file for writing." << std::endl;
-    return;
+  string number;
+  getline(count, number);
+  int id = 0;
+  char c;
+  for (int i = 0; i < number.size(); i++) {
+    c = number[i] - 47;
+    id = id * 10 + c;
   }
-  file_output << "[";
-  meta_data << "[";
-  // Accessing each article in json file
-  int i = 0;
-  for (const auto &entry : jsonData) {
-    unordered_map<string, int> mp;
-    split(entry["content"], mp);
-    nlohmann::json title, content;
-    content["id"] = title["id"] = i;
-    title["title"] = entry["title"];
-    title["url"] = entry["url"];
-    nlohmann::json word;
-    for (auto x : mp) {
-      word[x.first] = x.second;
-    }
-    content["words"] = word;
-    if (i == 0) {
-      file_output << content;
-      meta_data << title;
-    } else {
-      file_output << "," << content;
-      meta_data << "," << title;
-    }
-    i++;
-  }
-  file_output << "]";
-  meta_data << "]";
-  meta_data.close();
-  file_output.close();
-  file.close();
+  cout << id;
 }
+//   // Parsing json file
+//   nlohmann::json jsonData;
+//   file >> jsonData;
+
+//   ofstream file_output("index.json"), meta_data("metadata.json");
+//   if (!file_output.is_open() || !meta_data.is_open()) {
+//     std::cerr << "Failed to open the file for writing." << std::endl;
+//     return;
+//   }
+//   file_output << "[";
+//   meta_data << "[";
+//   // Accessing each article in json file
+//   int i = 0;
+//   for (const auto &entry : jsonData) {
+//     unordered_map<string, int> mp;
+//     split(entry["content"], mp);
+//     nlohmann::json title, content;
+//     content["id"] = title["id"] = i;
+//     title["title"] = entry["title"];
+//     title["url"] = entry["url"];
+//     nlohmann::json word;
+//     for (auto x : mp) {
+//       word[x.first] = x.second;
+//     }
+//     content["words"] = word;
+//     if (i == 0) {
+//       file_output << content;
+//       meta_data << title;
+//     } else {
+//       file_output << "," << content;
+//       meta_data << "," << title;
+//     }
+//     i++;
+//   }
+//   file_output << "]";
+//   meta_data << "]";
+//   meta_data.close();
+//   file_output.close();
+//   file.close();
+// }
 
 int main() {
   auto start_time = std::chrono::high_resolution_clock::now();
