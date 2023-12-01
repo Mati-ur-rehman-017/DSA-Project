fa1941ae865c7d75bbd3a5b56dd5b33f26d9e100
Shar-jeel-Sajid <135236134+Shar-jeel-Sajid@users.noreply.github.com>
2023-12-01 12:22:12 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,24 @@
 #include "src/forward_index.hpp"
+#include <filesystem>
 
 int main() {
-  auto start_time = std::chrono::high_resolution_clock::now();
-  forwardIndex("NewsData/weareanonymous.json");
-  auto end_time = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
-      end_time - start_time);
+  const std::string folderPath = "NewsData/";
 
-  std::cout << "Time taken by the first part: " << duration.count() / 1000
-            << " milliseconds" << std::endl;
+  try {
+    for (const auto &entry : std::filesystem::directory_iterator(folderPath)) {
+      if (entry.is_regular_file() && entry.path().extension() == ".json") {
+        auto start_time = std::chrono::high_resolution_clock::now();
+        forwardIndex(entry.path().filename());
+        auto end_time = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
+            end_time - start_time);
+        std::cout << "Time taken by the first part: "
+                  << duration.count() / 1000000 << " seconds" << std::endl;
+        std::cout << "File Name: " << entry.path().filename() << std::endl;
+      }
+    }
+  } catch (const std::exception &e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+  }
   return 0;
 }
