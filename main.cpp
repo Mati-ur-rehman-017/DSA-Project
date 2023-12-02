d001600a42d3f6bad5140a2c6e2737d1bfe24a2e
Mati-ur-rehman-017 <matiurrehamn017@gmail.com>
2023-12-02 19:55:40 +0500

    basic inverted with search function

--- main.cpp
+++ main.cpp
@@ -14,6 +14,16 @@ public:
     next = nullptr;
   }
 };
+class pairs{
+  public:
+  int id;
+  int score;
+  pairs(int ID, int Score)
+  {
+    id = ID;
+    score = Score;
+  }
+};
 
 void read_inverted(unordered_map<string,node*> & mp)
 {
@@ -134,23 +144,26 @@ void inverted_index(string a, unordered_map<string, node *> &mp)
   inverted.close();
 }
 
-void search_title(vector<int>&a,vector<vector<string>>&b){
+void search_title(vector<pairs*>&a,vector<vector<string>>&b){
   ifstream file("ForwardIndex\\metadata.txt");
   if (!file.is_open())
   {
     cout << "Can Not Open meta data file";
     return;
   }
-  
+  vector<string>d;
+  for(int i=0;i<a.size();i++){
+    b.push_back(d);
+  }
   char c=file.get();string id,title,url;
-  while (c!=EOF) {
+  while (!file.eof()) {
         if (c == '!') {
             id.clear();
             while (file.get(c) && c != '\\') {
                 id += c;
             }
             for (int i = 0; i < a.size(); i++) {
-                if (stoi(id) == a[i]) {
+                if (stoi(id) == a[i]->id) {
                     title.clear();
                     while (file.get(c) && c != ':') {
                         title += c;
@@ -172,48 +185,117 @@ void search_title(vector<int>&a,vector<vector<string>>&b){
     file.close(); 
 }
 
-void read_title(){
-  vector<int>a;vector<vector<string>>b;
-  a.push_back(0);
-  a.push_back(1);
-  vector<string>d;
-  for(int i=0;i<a.size();i++){
-    b.push_back(d);
+bool check_nullptr(vector<node*>&lists){
+  for(int i=0;i<lists.size();i++){
+    if(lists[i]==nullptr){return true;}
   }
-  search_title(a,b);
-  cout << endl << "Size of b: " << b.size() << endl;
-    for (int i = 0; i < b.size(); i++) {
-        cout << "Element " << i << ":\n";
-        for (int j = 0; j < b[i].size(); j++) {
-            cout << b[i][j] << "\n";
+  return false;
+}
+
+void greater_or_equal_tomax(vector<node*>&lists,int max){
+  for(int i=0;i<lists.size();i++){
+    while(lists[i]->id<max){
+      lists[i]=lists[i]->next;
+    }
+  }
+}
+
+int Partition(vector<pairs*> &v, int start, int end){
+	
+	int pivot = end;
+	int j = start;
+	for(int i=start;i<end;++i){
+		if(v[i]->score<v[pivot]->score){
+			swap(v[i],v[j]);
+			++j;
+		}
+	}
+	swap(v[j],v[pivot]);
+	return j;
+	
+}
+
+void Quicksort(vector<pairs*> &v, int start, int end ){
+
+	if(start<end){
+		int p = Partition(v,start,end);
+		Quicksort(v,start,p-1);
+		Quicksort(v,p+1,end);
+	}
+	
+}
+
+
+void search_words(vector<string>words,unordered_map<string,node*> & mp){
+  vector<node*>lists;
+  for(int i=0;i<words.size();i++){
+    if(mp[words[i]]==nullptr){
+      cout<<"\nNo Matches\n";return;
+    }
+    lists.push_back(mp[words[i]]);
+  }
+  //Finding id's with same words
+  vector<pairs*>ID;
+  while(!check_nullptr(lists)){
+    int id=lists[0]->id,max=-1;
+    for(int i=0;i<lists.size();i++){
+      if(id<lists[i]->id){
+        id=lists[i]->id;max=id;
+      }
+    }
+    if(max!=-1){
+      greater_or_equal_tomax(lists,max);
+    }else{
+      int sum=0;
+      for(int i=0;i<lists.size();i++){
+        sum+=lists[i]->score;
+      }
+      ID.push_back(new pairs(lists[0]->id,sum));
+      for(int i=0;i<lists.size();i++){
+        lists[i]=lists[i]->next;
+      }
+    }
+  }
+  //Sorting retrieved id's according to score
+  Quicksort(ID,0,ID.size()-1);
+  //Recieving titles and urls in a vector<vector<string>>
+  vector<vector<string>> info;
+  search_title(ID,info);
+  cout << endl << "Total no of articles: " << info.size() << endl;
+    for (int i = 0; i < info.size(); i++) {
+        cout << "Article " << i+1 << ":\n";
+        for (int j = 0; j < info[i].size(); j++) {
+            cout << info[i][j] << "\n";
         }
     }
 }
 
-void search_words(vector<string>words,unordered_map<string,node*> & mp){
-  
-}
+
 int main()
 {
-  unordered_map<string,node*>mp;
+  unordered_map<string,node*>mp;//read_inverted(mp);
   auto start_time = std::chrono::high_resolution_clock::now();
   // forwardIndex("369news.json");
   // inverted_index("ForwardIndex\\Index0.txt",mp);
-  // read_inverted(mp);
+  read_inverted(mp);
   //Checking loading inverted index
-  // node* temp3;
-  // for (auto x : mp) {
-  //     cout << '!' << x.first;
-  //     temp3=x.second;
-  //     while(temp3!=nullptr){
-  //       cout<<'\\'<<temp3->id<<':'<<temp3->score;
-  //       temp3=temp3->next;
-  //     }
-  // }
-  // read_forward("index");
-  read_title();
+// node* temp3;
+//   for (auto x : mp) {
+//       cout << '!' << x.first;
+//       temp3=x.second;
+//       while(temp3!=nullptr){
+//         cout<<'\\'<<temp3->id<<':'<<temp3->score;
+//         temp3=temp3->next;
+//       }
+  //}
+  vector<string>a;
+   a.push_back("pp");
+  // a.push_back("among");
+  search_words(a,mp);
+
+
   auto end_time = std::chrono::high_resolution_clock::now();
   auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
 
-  std::cout << "Time taken by the first part: " << duration.count() / 1000000 << "seconds" << std::endl;
+  std::cout << "Time taken by the first part: " << duration.count() / 1000 << "miliseconds" << std::endl;
 }
